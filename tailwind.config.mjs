5e46081420d3cc015c6c5cdef40301b5ca7e3772
AlejandroSenior <alejandrosenior@outlook.es>
2024-03-07 16:58:24 +0100

    Tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D0D6F9',
+        secondary: '#0B0D17'
+      }
+    }
+  }
+};
